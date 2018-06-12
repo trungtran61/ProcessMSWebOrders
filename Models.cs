59914354b800f962d85ccd5b080d2848a3d30570
trungtran61 <trungtran61@gmail.com>
2018-06-11 23:29:23 -0700

    initial commit

---
+++ Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProcessMSWebOrders
+{
+    class Models
+    {
+        public class Order
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string phone { get; set; }
+            public DateTime pickUpTime { get; set; }
+            public DateTime pickUpDate { get; set; }
+            public OrderItem[] orderItems { get; set; }
+        }
+
+        public class OrderItem
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public int qty { get; set; }
+            public decimal price { get; set; }
+            public string instructions { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,70 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Net.Http;
+using static ProcessMSWebOrders.Models;
 
-namespace ProcessMSWebOrders
+namespace ProcessWebOrders
 {
     class Program
     {
+        private static List<string> printLines = new List<string>();
+
         static void Main(string[] args)
         {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(ConfigurationManager.AppSettings["GetOrdersURI"]).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+                    // by calling .Result you are synchronously reading the result
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    List<Order> Orders = JsonConvert.DeserializeObject<List<Order>>(responseString);
+
+                    foreach (Order order in Orders)
+                    {
+                        printLines.Clear();
+                        printLines.Add(order.name);
+                        printLines.Add(order.phone);
+                        printLines.Add(order.pickUpTime.ToShortTimeString());
+                        foreach (OrderItem item in order.orderItems)
+                        {
+                            printLines.Add(string.Format("{0} ({1})", item.name, item.qty));
+                            printLines.Add(item.instructions == null? "": item.instructions);                            
+                        }
+                        Print(ConfigurationManager.AppSettings["Printer"]);
+                    }
+                }
+            }
+
+        }
+
+        static void Print(string PrinterName)
+        {
+            PrintDocument doc = new PrintDocument();
+            doc.PrinterSettings.PrinterName = PrinterName;
+            doc.PrintPage += new PrintPageEventHandler(PrintHandler);
+            doc.Print();
+        }
+
+        private static void PrintHandler(object sender, PrintPageEventArgs ppeArgs)
+        {
+            int xpos = 10;
+            int ypos = 20;
+            Font FontNormal = new Font("Verdana", 16, FontStyle.Bold);
+            Graphics g = ppeArgs.Graphics;
+
+            foreach (string line in printLines)
+            {
+                g.DrawString(line.ToUpper() + Environment.NewLine, FontNormal, Brushes.Black, xpos, ypos, new StringFormat());
+                ypos += 30;
+            }
         }
     }
 }
+
